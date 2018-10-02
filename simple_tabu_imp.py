2660d0587a8c3d44c7eed57b3507b01a557ff914
Christian <c.atallah2@newcastle.ac.uk>
2018-10-02 10:23:38 +0100

    initial commit

---
+++ simple_tabu_imp.py
@@ -0,0 +1,8 @@
+
+from tabstuSearch.TabuSearch import TabuSearch as TabuSearch
+from tabstuSearch.Solution import Solution as Solution
+
+class MEnzDPTabuSearch(TabuSearch):
+
+    def _score(self, sol):
+        
